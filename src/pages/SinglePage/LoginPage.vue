0af829ca4ece9662aef93a624beea95183b06536
zmaplex <zmaplex@gmail.com>
2022-08-15 20:50:59 +0800

    add warning tips

--- src/pages/SinglePage/LoginPage.vue
+++ src/pages/SinglePage/LoginPage.vue
@@ -1,31 +1,43 @@
 <template>
   <q-layout>
     <q-page class="flex flex-center full-height full-width bg-blue-grey-1">
-      <q-card class="apple-card card">
-        <q-card-section class="bg-blue-grey-2">
-          <div class="text-body1 text-center text-capitalize">login your server</div>
-        </q-card-section>
+      <div class="q-gutter-sm">
+        <q-card class="apple-card card">
+          <q-card-section class="bg-blue-grey-2">
+            <div class="text-body1 text-center text-capitalize">login your server</div>
 
-        <q-card-section class="q-pa-lg q-gutter-sm">
-          <q-input v-model="data.apiUrl" color="blue-grey" dense label="API" @update:model-value="updateApiUrl">
-            <template v-slot:append>
-              <q-btn flat icon="done" @click="setApiUrl()">
-                <q-tooltip>
-                  Click to take effect
-                </q-tooltip>
-              </q-btn>
-            </template>
-          </q-input>
-          <div v-if="ui.showForm">
-            <q-input v-model="data.loginForm.username" color="blue-grey" label="username"></q-input>
-            <q-input v-model="data.loginForm.password" color="blue-grey" label="password" type="password"></q-input>
+          </q-card-section>
+
+          <q-card-section class="q-pa-lg q-gutter-sm ">
+
+            <q-input v-model="data.apiUrl" :color="showWarning()?'warning':'blue-grey'" dense label="API"
+                     @update:model-value="updateApiUrl">
+              <template v-slot:append>
+                <q-btn flat icon="done" @click="setApiUrl()">
+                  <q-tooltip>
+                    Click to take effect
+                  </q-tooltip>
+                </q-btn>
+              </template>
+            </q-input>
+            <div v-if="ui.showForm">
+              <q-input v-model="data.loginForm.username" color="blue-grey" label="username"></q-input>
+              <q-input v-model="data.loginForm.password" color="blue-grey" label="password" type="password"></q-input>
+            </div>
+          </q-card-section>
+          <q-card-actions v-if="ui.showForm" align="between" class="q-pa-md">
+            <q-btn :loading="ui.loginBtn.loading" color="blue" label="login" @click="onLogin"></q-btn>
+            <q-btn color="grey" label="forget password"></q-btn>
+          </q-card-actions>
+
+        </q-card>
+        <q-banner v-if="showWarning()" dense rounded class="text-center bg-transparent ">
+          <div style="opacity:0.6">
+            <q-icon name="o_lock_open" size="18px" color="red"></q-icon>
+            <span class="q-ml-sm text-red  text-caption">Data is not encrypted during transmission.</span>
           </div>
-        </q-card-section>
-        <q-card-actions v-if="ui.showForm" align="between" class="q-pa-md">
-          <q-btn :loading="ui.loginBtn.loading" color="blue" label="login" @click="onLogin"></q-btn>
-          <q-btn color="grey" label="forget password"></q-btn>
-        </q-card-actions>
-      </q-card>
+        </q-banner>
+      </div>
     </q-page>
   </q-layout>
 </template>
@@ -69,6 +81,7 @@ export default {
         data.value.apiUrl = route.query.apiUrl
       }
     })
+
     function updateApiUrl(val) {
       ui.value.showForm = window.localStorage.getItem("api_url") === data.value.apiUrl
     }
@@ -80,6 +93,14 @@ export default {
     function setApiUrl() {
       window.localStorage.setItem("api_url", data.value.apiUrl)
       window.location.href = "/"
+    }
+
+    function showWarning() {
+      if (data.value.apiUrl === "/") {
+        return window.location.protocol === "http:"
+      } else {
+        return !data.value.apiUrl.includes("https://")
+      }
     }
 
     async function onLogin() {
@@ -96,7 +117,7 @@ export default {
 
     }
 
-    return {ui, data, onLogin, setApiUrl, getCurrentApi, updateApiUrl}
+    return {ui, data, onLogin, setApiUrl, getCurrentApi, updateApiUrl, showWarning}
   }
 }
 </script>
